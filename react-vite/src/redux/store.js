8c1fe9038017d25ae6794aa0999cbd9a92f9a8e3
brodrickbaker <brodrickrbaker@gmail.com>
2024-12-17 20:56:19 -0500

    removed data to test deployment

--- react-vite/src/redux/store.js
+++ react-vite/src/redux/store.js
@@ -6,9 +6,11 @@ import {
 } from "redux";
 import thunk from "redux-thunk";
 import sessionReducer from "./session";
+import selectReducer from "./selected";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
+  selected: selectReducer
 });
 
 let enhancer;
