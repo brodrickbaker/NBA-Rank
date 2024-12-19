1223e93ab84a32dfa2dd8b10f563d3bf662a033a
brodrickbaker <brodrickrbaker@gmail.com>
2024-12-19 09:17:07 -0500

    updated seeder to test deployment

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,11 +5,11 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', email='demo@aa.io', password='password', about='about Demo')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie', email='marnie@aa.io', password='password', about='about Marnie')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='bobbie', email='bobbie@aa.io', password='password', about='about Bobbie')
 
     db.session.add(demo)
     db.session.add(marnie)
